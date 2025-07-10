8d216032999c1ccf09f5a4260dabf56bd53a11ad
CreativeMarkus <mark@creativemarkus.com>
2025-07-10 11:00:51 -0400

    updated the main view

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import { BookCard } from "../book-card/book-card";
 import { BookView } from "../book-view/book-view";
 
-
-
 export const MainView = () => {
     const [books, setBooks] = useState([
         {
@@ -11,36 +9,36 @@ export const MainView = () => {
             title: "Eloquent JavaScript",
             image:
                 "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
-            author: "Marijn Haverbeke"
+            author: "Marijn Haverbeke",
         },
         {
             id: 2,
             title: "Mastering JavaScript Functional Programming",
             image:
                 "https://images-na.ssl-images-amazon.com/images/I/51WAikRq37L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-            author: "Federico Kereki"
+            author: "Federico Kereki",
         },
         {
             id: 3,
             title: "JavaScript: The Good Parts",
             image:
                 "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg",
-            author: "Douglas Crockford"
+            author: "Douglas Crockford",
         },
         {
             id: 4,
             title: "JavaScript: The Definitive Guide",
             image:
                 "https://images-na.ssl-images-amazon.com/images/I/51HbNW6RzhL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-            author: "David Flanagan"
+            author: "David Flanagan",
         },
         {
             id: 5,
             title: "The Road to React",
             image:
                 "https://images-na.ssl-images-amazon.com/images/I/41MBLi5a4jL._SX384_BO1,204,203,200_.jpg",
-            author: "Robin Wieruch"
-        }
+            author: "Robin Wieruch",
+        },
     ]);
 
     const [selectedBook, setSelectedBook] = useState(null);
@@ -55,7 +53,6 @@ export const MainView = () => {
 
     return (
         <div>
-            {/* Button appears above the book list */}
             <button
                 onClick={() => {
                     alert("Nice!");
