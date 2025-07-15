4cc69087b148f6e9fffff02afedf9c39963c30e3
CreativeMarkus <mark@creativemarkus.com>
2025-07-15 14:56:24 -0400

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import PropTypes from 'prop-types';
 
 export const MovieCard = ({ movie, onMovieClick }) => {
-    return <div onClick={() => onMovieClick(movie)}>{movie.title}</div>;
+  return (
+    <div onClick={() => onMovieClick(movie)}>
+      <img src={movie.ImagePath} alt={movie.Title} style={{ width: '150px' }} />
+      <h3>{movie.Title}</h3>
+    </div>
+  );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
