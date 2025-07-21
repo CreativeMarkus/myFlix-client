c619f2785992c6f07bf099b5b8b5a809f82ed3c4
CreativeMarkus <mark@creativemarkus.com>
2025-07-21 12:07:23 -0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,17 @@
-// src/index.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainView } from "./components/MainView/main-view";
+import { MovieView } from "./components/MovieView/movie-view"; // Create this if needed
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<MainView />);
+
+root.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<MainView />} />
+            <Route path="/movies/:movieId" element={<MovieView />} />
+        </Routes>
+    </BrowserRouter>
+);
