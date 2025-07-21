887885327fa2c257c079986b58000338df1fee13
CreativeMarkus <mark@creativemarkus.com>
2025-07-21 10:20:41 -0400

    Make /movies public and update CORS

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -1,53 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movies] = useState([
-        {
-            id: 1,
-            title: "Inception",
-            description: "A skilled thief enters people's dreams to steal ideas.",
-            image: "https://m.media-amazon.com/images/I/51v5ZpFyaFL._AC_.jpg",
-            genre: "Sci-Fi",
-            director: "Christopher Nolan",
-        },
-        {
-            id: 2,
-            title: "The Matrix",
-            description: "A hacker discovers reality is a simulation.",
-            image: "https://m.media-amazon.com/images/I/51EG732BV3L._AC_.jpg",
-            genre: "Action",
-            director: "The Wachowskis",
-        },
-        {
-            id: 3,
-            title: "Interstellar",
-            description: "A team explores a wormhole to save humanity.",
-            image:
-                "https://resizing.flixster.com/7c3qnZfPzZgID7Ft97PccFwEf9U=/206x305/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p10543523_p_v8_as.jpg",
-            genre: "Sci-Fi",
-            director: "Christopher Nolan",
-        },
-        {
-            id: 4,
-            title: "The Dark Knight",
-            description: "Batman faces the Joker, a criminal mastermind wreaking havoc in Gotham.",
-            image: "https://m.media-amazon.com/images/I/71dwS9phhCL.jpg",
-            genre: "Action",
-            director: "Christopher Nolan",
-        },
-        {
-            id: 5,
-            title: "Avatar",
-            description: "A paraplegic Marine is sent to Pandora and becomes torn between two worlds.",
-            image: "https://m.media-amazon.com/images/I/41kTVLeW1CL._AC_.jpg",
-            genre: "Sci-Fi",
-            director: "James Cameron",
-        }
-    ]);
-
+    const [movies, setMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetch("https://movieapi1-683469e1d996.herokuapp.com/movies")
+            .then((response) => response.json())
+            .then((data) => setMovies(data))
+            .catch((error) => console.error("Error fetching movies:", error));
+    }, []);
 
     if (selectedMovie) {
         return (
@@ -62,11 +26,23 @@ export const MainView = () => {
         <div>
             {movies.map((movie) => (
                 <MovieCard
-                    key={movie.id}
+                    key={movie._id}
                     movie={movie}
-                    onMovieClick={setSelectedMovie}
+                    onMovieClick={(movie) => setSelectedMovie(movie)}
                 />
             ))}
         </div>
     );
+};
+import PropTypes from 'prop-types';
+
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        genre: PropTypes.string.isRequired,
+        director: PropTypes.string.isRequired,
+    }).isRequired,
+    onBackClick: PropTypes.func.isRequired,
 };
