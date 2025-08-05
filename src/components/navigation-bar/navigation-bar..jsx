61e5135b4939e6761d9291f4ac58d760011f4931
CreativeMarkus <mark@creativemarkus.com>
2025-08-05 14:04:12 -0400

    Update navigation-bar..jsx

--- src/components/navigation-bar/navigation-bar..jsx
+++ src/components/navigation-bar/navigation-bar..jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
-                <Navbar.Brand href="/">MyFlix</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/">MyFlix</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        {user && (
+                        {user ? (
                             <>
-                                <Nav.Link href="/profile">Profile</Nav.Link>
+                                <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
                                 <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
                             </>
-                        )}
-                        {!user && (
+                        ) : (
                             <>
-                                <Nav.Link href="/login">Login</Nav.Link>
-                                <Nav.Link href="/signup">Signup</Nav.Link>
+                                <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                                <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
                             </>
                         )}
                     </Nav>
